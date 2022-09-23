c73ba364687fc08f6ec5391687a813e77ce01970
Aariq123 <michealscarn09@gmail.com>
2022-09-23 20:11:16 +0600

    4th commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
          
             <ul className={navOpen ? 'nav-ul open' : 'nav-ul'}>
                 <div className="links">
-                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/modaHou'>Home</Link></li>
                     <li><Link to='/plans'>Our Plans</Link></li>
                     <li><Link to='/menu'>Menus</Link></li>
                 </div>
